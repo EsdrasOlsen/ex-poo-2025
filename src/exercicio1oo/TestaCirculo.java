a11cf630d18dc175b4525fb46996ec7afff85fc1
Esdras <esdraske@gmail.com>
2025-04-14 20:47:48 -0300

    Criação de Circulo e TestaCirculo

---
+++ src/exercicio1oo/TestaCirculo.java
@@ -0,0 +1,9 @@
+package exercicio1oo;
+
+public class TestaCirculo {
+    public static void main(String[] args) {
+        Circulo bolinha = new Circulo();
+        bolinha.raio = 13.5;
+        System.out.println(bolinha.raio);
+    }
+}
