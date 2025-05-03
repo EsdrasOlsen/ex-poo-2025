d1d06479dc0891e3ebfdba96390659d6ea5093ab
Esdras <esdraske@gmail.com>
2025-05-03 16:30:08 -0300

    Adicionado o calculo da area do retangulo, juntamente com sua impressão.

--- src/exercicio4oo/classes/Retangulo.java
+++ src/exercicio4oo/classes/Retangulo.java
@@ -1,9 +1,14 @@
 package exercicio4oo.classes;
 
+import java.awt.geom.Area;
+
 public class Retangulo {
     private double largura;
     private double altura;
 
+    public double calcularArea() {
+        return this.largura * this.altura;
+    }
     public Double getLargura() {return largura;}
     public void setLargura(double largura) {this.largura = largura;}
 
--- src/exercicio4oo/testes/TestaRetangulo.java
+++ src/exercicio4oo/testes/TestaRetangulo.java
@@ -7,6 +7,6 @@ public class TestaRetangulo {
         Retangulo r1 = new Retangulo();
         r1.setLargura(3.0);
         r1.setAltura(4.0);
-        System.out.printf("Largura = %.2f - Altura = %.2f \n", r1.getLargura(), r1.getAltura());
+        System.out.printf("Largura = %.2f - Altura = %.2f - Area do Retangulo: %.2f\n", r1.getLargura(), r1.getAltura(),r1.calcularArea());
     }
 }
