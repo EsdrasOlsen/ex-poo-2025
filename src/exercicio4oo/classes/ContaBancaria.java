891a1dbfff5140608437d50defb13a3ae3c943c7
Esdras <esdraske@gmail.com>
2025-05-03 18:05:44 -0300

    Adicionado o metodo de saque, deposito e após isso mostrar o saldo atual.

--- src/exercicio4oo/classes/ContaBancaria.java
+++ src/exercicio4oo/classes/ContaBancaria.java
@@ -5,6 +5,34 @@ public class ContaBancaria {
     private String titular;
     private double saldo;
 
+    //depositos
+    public void depositar(double valor){
+        if (valor > 0){
+            saldo += valor;
+            System.out.printf("\n Deposito de R$" + valor + " realizado com sucesso!\n");
+        }
+        else{
+            System.out.printf("Valor de depósito inválido.");
+        }
+    }
+
+    //Saques
+    public void sacar(double valor){
+        if (valor > 0){
+            saldo -= valor;
+            System.out.println("\n Saque realizado com sucesso!\n");
+        }
+        else{
+            System.out.println("Saque inválido. Verifique o saldo ou o valor digitado.");
+        }
+    }
+
+    //conultar saldo
+
+    public double consultarSaldo(){
+        return saldo;
+    }
+
     public String getNumeroConta() {return numeroConta;}
     public void setNumeroConta(String numeroConta) {this.numeroConta = numeroConta;}
 
--- src/exercicio4oo/testes/TestaContaBancaria.java
+++ src/exercicio4oo/testes/TestaContaBancaria.java
@@ -5,9 +5,11 @@ import exercicio4oo.classes.ContaBancaria;
 public class TestaContaBancaria {
     public static void main(String[] args) {
         ContaBancaria banco = new ContaBancaria();
-        banco.setSaldo(65.000);
+        banco.setSaldo(65000.0);
         banco.setTitular("Fulano");
         banco.setNumeroConta("123456789");
+        banco.depositar(1000.0);
+        banco.sacar(2000.0);
         System.out.printf("Saldo: %.2f - Titular: %s - Numero da Conta: %s\n",banco.getSaldo(),banco.getTitular(),banco.getNumeroConta());
     }
 }
